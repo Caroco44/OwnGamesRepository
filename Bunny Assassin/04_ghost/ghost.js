cfa7780f73337e0c52110f9052c03bd784dc7ae8
Caroco44 <caroline04kraemer@yahoo.de>
2024-02-26 12:17:31 +0100

    BunnyAssassin_240226_3

--- Bunny Assassin/04_ghost/ghost.js
+++ Bunny Assassin/04_ghost/ghost.js
@@ -1,1 +1,108 @@
 
+document.getElementById('littleB01').addEventListener('click', function () {
+    var ghostStatement = document.getElementById('ghostStatement01');
+    
+    // Show ghostStatement
+    ghostStatement.style.display = 'block';
+
+    // Hide ghostStatement after 2 seconds
+    setTimeout(function () {
+      ghostStatement.style.display = 'none';
+    }, 3000);
+  });
+
+  document.getElementById('littleB02').addEventListener('click', function () {
+    var ghostStatement = document.getElementById('ghostStatement02');
+    
+    // Show ghostStatement
+    ghostStatement.style.display = 'block';
+
+    // Hide ghostStatement after 2 seconds
+    setTimeout(function () {
+      ghostStatement.style.display = 'none';
+    }, 3000);
+  });
+
+  document.getElementById('littleB03').addEventListener('click', function () {
+    var ghostStatement = document.getElementById('ghostStatement03');
+    
+    // Show ghostStatement
+    ghostStatement.style.display = 'block';
+
+    // Hide ghostStatement after 2 seconds
+    setTimeout(function () {
+      ghostStatement.style.display = 'none';
+    }, 5000);
+  });
+
+  document.getElementById('littleB04').addEventListener('click', function () {
+    var ghostStatement = document.getElementById('ghostStatement04');
+    
+    // Show ghostStatement
+    ghostStatement.style.display = 'block';
+
+    // Hide ghostStatement after 2 seconds
+    setTimeout(function () {
+      ghostStatement.style.display = 'none';
+    }, 3000);
+  });
+
+  document.getElementById('littleB05').addEventListener('click', function () {
+    var ghostStatement = document.getElementById('ghostStatement05');
+    
+    // Show ghostStatement
+    ghostStatement.style.display = 'block';
+
+    // Hide ghostStatement after 2 seconds
+    setTimeout(function () {
+      ghostStatement.style.display = 'none';
+    }, 3000);
+  });
+
+  document.getElementById('littleB06').addEventListener('click', function () {
+    var ghostStatement = document.getElementById('ghostStatement06');
+    
+    // Show ghostStatement
+    ghostStatement.style.display = 'block';
+
+    // Hide ghostStatement after 2 seconds
+    setTimeout(function () {
+      ghostStatement.style.display = 'none';
+    }, 3000);
+  });
+
+  document.getElementById('littleB07').addEventListener('click', function () {
+    var ghostStatement = document.getElementById('ghostStatement07');
+    
+    // Show ghostStatement
+    ghostStatement.style.display = 'block';
+
+    // Hide ghostStatement after 2 seconds
+    setTimeout(function () {
+      ghostStatement.style.display = 'none';
+    }, 5000);
+  });
+
+  document.getElementById('littleB08').addEventListener('click', function () {
+    var ghostStatement = document.getElementById('ghostStatement08');
+    
+    // Show ghostStatement
+    ghostStatement.style.display = 'block';
+
+    // Hide ghostStatement after 2 seconds
+    setTimeout(function () {
+      ghostStatement.style.display = 'none';
+    }, 5000);
+  });
+
+  document.getElementById('littleB09').addEventListener('click', function () {
+    var ghostStatement = document.getElementById('ghostStatement09');
+    
+    // Show ghostStatement
+    ghostStatement.style.display = 'block';
+
+    // Hide ghostStatement after 2 seconds
+    setTimeout(function () {
+      ghostStatement.style.display = 'none';
+    }, 3000);
+  });
